6332e09d99f96b885b5981dde5df9f4216f4257b
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-03-30 23:09:09 -0700

    Add reactJQuery idea for fade in

--- reactJQuery/hideFadeIn.jsx
+++ reactJQuery/hideFadeIn.jsx
@@ -12,3 +12,5 @@ function App() {
 }
 
 export default App;
+
+// $('header').hide().fadeIn(1500) is an example of how to fade in a header after 1500 milliseconds. Or, you can write $('header').hide().delay(500).fadeIn(1500) to do the same but with a 5 second delay in the middle.
